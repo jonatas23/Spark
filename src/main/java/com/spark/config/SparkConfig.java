423ff675383db2a622665dd8f871f5aa87c7cbf9
jonatas_oliveira <jonatas.oliveira@sgs.com>
2019-09-04 16:16:04 -0300

    Projeto Basico Spark

---
+++ src/main/java/com/spark/config/SparkConfig.java
@@ -0,0 +1,44 @@
+package com.spark.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.serializer.KryoSerializer;
+import org.apache.spark.sql.SparkSession;
+import org.datasyslab.geospark.serde.GeoSparkKryoRegistrator;
+import org.datasyslab.geosparksql.utils.GeoSparkSQLRegistrator;
+import org.datasyslab.geosparkviz.sql.utils.GeoSparkVizRegistrator;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+public class SparkConfig {
+
+	@Value("${spark.app.name}")
+	private String appName;
+
+	@Value("${spark.master}")
+	private String masterUri;
+
+	@Bean
+	public SparkSession sparkSession() {
+		SparkSession sparkSession = SparkSession.builder()
+				.appName(appName)
+				.master(masterUri)
+				.config("spark.ui.port","4041")
+				.config("spark.serializer", KryoSerializer.class.getName())
+				.config("spark.kryo.registrator", GeoSparkKryoRegistrator.class.getName())
+				.getOrCreate();
+
+		GeoSparkSQLRegistrator.registerAll(sparkSession);
+		GeoSparkVizRegistrator.registerAll(sparkSession);
+		return sparkSession;
+	}
+
+	@Bean
+	public JavaSparkContext javaSparkContext() {
+		return new JavaSparkContext(sparkSession().sparkContext());
+	}
+
+}
